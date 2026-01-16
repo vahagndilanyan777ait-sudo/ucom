dbf237410216fbede92d1232286cba426f314303
vahagndilanyan777ait-sudo <vahagn.dilanyan777.ait@gmail.com>
2026-01-16 18:12:38 +0400

    Ubdate ucomLogin style

--- src/components/ucomlogin.jsx
+++ src/components/ucomlogin.jsx
@@ -1,61 +1,63 @@
 
-import "./UcomLogin.css";
+import "./ucomlogin.css";
 
 
 export default function UcomLogin() {
-return (
-<div className="page">
-<div className="container">
-<div className="card">
-<h1>Լիցքավորեք Ձեր հաշվին առանց</h1>
-<p className="subtitle">
-Մուտքագրեք Ձեր Ucom շարժական կամ ֆիքսված ծառայությունների համարը
-</p>
+    return (
+        <div className="page">
+            <div className="container">
+                <div className="card">
+                    <h1>Լիցքավորեք Ձեր հաշվին առանց</h1>
+                    <p className="subtitle">
+                        Մուտքագրեք Ձեր Ucom շարժական կամ ֆիքսված ծառայությունների համարը
+                    </p>
 
 
-<label className="label">Հեռախոսահամար / ID</label>
-<div className="phone-row">
-<div className="code">+374</div>
-<input placeholder="XX XXX XXX" />
-</div>
+                    <label className="label">Հեռախոսահամար / ID</label>
+                    <div className="phone-row">
+                        <div className="code">+374</div>
+                        <input placeholder="XX XXX XXX" />
+                    </div>
 
 
-<button className="pay-btn">Վճարել</button>
+                    <button className="pay-btn">Վճարել</button>
 
 
-<div className="cards">
-<span className="visa">VISA</span>
-<span className="mc">Mastercard</span>
-</div>
-</div>
+                    <div className="cards">
+                        <span className="visa">VISA</span>
+                        <span className="mc">Mastercard</span>
+                    </div>
+                </div>
 
 
 
-<div className="card">
-<h2>Ucom հավելվածներ</h2>
-<p className="subtitle">
-Բացահայտեք հարմարավետության նոր մակարդակ մեր թվային լուծումների միջոցով
-</p>
+                <div className="card">
+                    <h2>Ucom հավելվածներ</h2>
+                    <p className="subtitle">
+                        Բացահայտեք հարմարավետության նոր մակարդակ մեր թվային լուծումների միջոցով
+                    </p>
 
 
-<div className="apps">
-<AppItem title="Ucom" />
-<AppItem title="Uplay" />
-<AppItem title="UPay" />
-<AppItem title="uKid" />
-</div>
-</div>
-</div>
-</div>
-);
-}
-
-
-function AppItem({ title }) {
-return (
-<div className="app-item">
-<div className="icon">{title[0]}</div>
-<span>{title}</span>
-</div>
-);
+                    <div className="apps">
+                        <div className="app-item">
+                            <img className="icon" src="https://www.ucom.am/storage/files/rectangle-513-1-48x_-quality(75)-webp(80).png?token=23649e93ba54ee148b059dbedf9e2efe" />
+                            <span>Ucom </span>
+                        </div>
+                        <div className="app-item">
+                            <img className="icon" src="https://www.ucom.am/storage/files/285x285-48x_-quality(75)-webp(80).png?token=5e834fd78f91e64b19f4e20a77b2d3de" />
+                            <span>Uplay</span>
+                        </div>
+                        <div className="app-item">
+                            <img className="icon"  src="https://www.ucom.am/storage/files/new-upay.svg" />
+                            <span>UPay</span>
+                        </div>
+                        <div className="app-item">
+                            <img className="icon" src="https://www.ucom.am/storage/files/ukid_1.svg" />
+                            <span>uKid</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
