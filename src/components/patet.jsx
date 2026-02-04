67959e11013581733598e7c0f639221a707be049
vahagndilanyan777ait-sudo <vahagn.dilanyan777.ait@gmail.com>
2026-02-04 16:50:51 +0400

    patet card and card to tailwind css

--- src/components/patet.jsx
+++ src/components/patet.jsx
@@ -1,52 +1,53 @@
-import React from 'react'
-import Patetcard from './patetcard'
-import "./patet.css"
+import React from "react";
+import PatetCard from "./PatetCard";
 
-export default function patet() {
-    
-    let db = [
-        {
-            name:'Level up+ 3000',
-            internet:"30 ԳԲ ինտերնետ",
-            rope:"1500 րոպե",
-            sms:"SMS 300",
-            aliq:"35 հեռուստաալիք՝ Uplay հարթակում",
-            gin:"3000 ֏",
-        },
-        {
-            name:'Level up+ 4000',
-            internet:"Անսահմանափակ ինտերնետ",
-            rope:"700 րոպե",
-            sms:"SMS 500",
-            aliq:"35 հեռուստաալիք՝ Uplay հարթակում",
-            gin:"4000 ֏",
-        },
-         {
-            name:'Level up+ 5000',
-            internet:"Անսահմանափակ ինտերնետ",
-            rope:"3000 րոպե",
-            sms:"SMS 1000",
-            aliq:"35 հեռուստաալիք՝ Uplay հարթակում",
-            gin:"5000 ֏",
-        },
-         {
-            name:'Unity 12500',
-            internet:"Անսահմանափակ ինտերնետ",
-            rope:"3000 րոպե",
-            sms:"SMS 1000",
-            aliq:"200+ հեռուստաալիք՝ Uplay հարթակում",
-            gin:"12500 ֏",
-        },
-    ]
+export default function Patet() {
+  const db = [
+    {
+      name: "Level up+ 3000",
+      internet: "30 ԳԲ ինտերնետ",
+      rope: "1500 րոպե",
+      sms: "SMS 300",
+      aliq: "35 հեռուստաալիք՝ Uplay հարթակում",
+      gin: "3000 ֏",
+    },
+    {
+      name: "Level up+ 4000",
+      internet: "Անսահմանափակ ինտերնետ",
+      rope: "700 րոպե",
+      sms: "SMS 500",
+      aliq: "35 հեռուստաալիք՝ Uplay հարթակում",
+      gin: "4000 ֏",
+    },
+    {
+      name: "Level up+ 5000",
+      internet: "Անսահմանափակ ինտերնետ",
+      rope: "3000 րոպե",
+      sms: "SMS 1000",
+      aliq: "35 հեռուստաալիք՝ Uplay հարթակում",
+      gin: "5000 ֏",
+    },
+    {
+      name: "Unity 12500",
+      internet: "Անսահմանափակ ինտերնետ",
+      rope: "3000 րոպե",
+      sms: "SMS 1000",
+      aliq: "200+ հեռուստաալիք՝ Uplay հարթակում",
+      gin: "12500 ֏",
+    },
+  ];
 
   return (
-    <div className='patet'>
-        <h1>Լավագույն առաջարկ</h1>
-        <div className='patetCards'>
-            {db.map((el,i)=>{
-                return <Patetcard name={el.name} aliq={el.aliq} internet={el.internet} gin={el.gin} sms={el.sms} rope={el.rope} />
-            })}
-        </div>
+    <div className="mt-[100px] flex w-full flex-col justify-center gap-5 p-[100px]">
+      <h1 className="text-3xl font-bold">Լավագույն առաջարկ</h1>
+
+      <div className="flex w-full gap-5">
+        {db.map((el, i) => (
+          <PatetCard key={i} {...el} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
+
+
--- src/components/patetcard.jsx
+++ src/components/patetcard.jsx
@@ -1,39 +1,36 @@
-import React from 'react'
+export default function PatetCard({
+  name,
+  internet,
+  rope,
+  sms,
+  aliq,
+  gin,
+}) {
+  return (
+    <div className="flex h-[470px] w-[318px] flex-col items-center rounded-[15px] border border-gray-400 transition hover:border-green-400">
+      
+      {/* Header */}
+      <div className="flex h-[60px] w-full items-center rounded-t-[15px] bg-[#7ebc0a] pl-3 text-[15px] font-bold text-white">
+        {name}
+      </div>
 
-export default function patetcard({ name, internet, rope, sms, aliq, gin }) {
-    return (
-        <div className='patetCard'>
-            <div className="anun">
-                {name}
-            </div>
-            <div className="info">
-                <p>
-                    <i class="fa-solid fa-globe"></i>
-                    {internet}
-                </p>
-                <p>
-                    <i class="fa-solid fa-phone-volume"></i>
-                    {rope}
-                </p>
-                <p>
-                    <i class="fa-regular fa-comment-dots"></i>
-                    {sms}
-                </p>
-                <p>
-                    <i class="fa-solid fa-tv"></i>
-                    {aliq}
-                </p>
+      {/* Info */}
+      <div className="flex flex-col gap-1 p-[5%] text-sm">
+        <p>{internet}</p>
+        <p>{rope}</p>
+        <p>{sms}</p>
+        <p>{aliq}</p>
+      </div>
 
-            </div>
-            <div className="gumar">
-                <p>
-                    {gin}
-                </p>
-                <p>
-                    Ավելին
-                </p>
-            </div>
-            <button>Միանալ</button>
-        </div>
-    )
+      {/* Price */}
+      <div className="mt-auto flex w-full items-center justify-around pb-4 text-lg font-bold">
+        {gin}
+      </div>
+
+      {/* Button */}
+      <button className="mb-4 flex h-[60px] w-[90%] items-center justify-center rounded-[15px] bg-[#7ebc0a] text-[15px] font-bold text-black transition hover:bg-green-400">
+        Միանալ
+      </button>
+    </div>
+  );
 }
